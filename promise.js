bdf31fafae497346d5b3a745a928f2122e02cdcb
DiaaSaada <dia.saada@gmail.com>
2020-12-08 17:20:27 +0300

    promise with catch

--- promise.js
+++ promise.js
@@ -19,3 +19,38 @@ promise.then((user) => {
     console.log(error)
 })
 console.log("A2")
+
+
+//  async & wait
+
+const fetchArticle = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ title: "longTask1" })
+        }, 2000)
+    })
+}
+
+const fetchAuthor = (article) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ author_name: "author" })
+        }, 2000)
+    })
+}
+
+const fetchAuthorCountry = (author) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ country_name: "Turkey" })
+        }, 2000)
+    })
+}
+
+
+console.log('promise unclean way')
+    // un clean way
+const res = fetchArticle()
+    .then(article => fetchAuthor(article))
+    .then(author => fetchAuthorCountry(author))
+    .then(authorCountry => console.log(authorCountry));
